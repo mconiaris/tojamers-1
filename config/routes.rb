4f4f41ea0dd168f26f9dde9776d6611c98f6fa08
mconiaris <mconiaris@gmail.com>
2014-10-02 19:12:54 -0400

    Created StoriesController

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/login' => 'sessions#new'
   resource :session, only: [:create, :destroy]
+  resources :stories
   resources :users do
     member do
       get "password"
       patch "password", action: "update password"
       get "confirm_delete"
     end
-
   end
 
 
