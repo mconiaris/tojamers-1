ca36bcd0f90d841bcd7556138504f6a89ad61590
ja9harper <ja9harper@gmail.com>
2014-10-02 18:04:20 -0400

    Added new middle name to Keith Moon

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ toro    = User.create(name: "Toro Rojo", email: "toro@nasa.gov", company: true, 
 obama  = User.create(name: "Obama", email: "obama@gmail.com", company: true, phone: "1-900-ilv-taxes", password: "fourmoreyears")
 chuck  = User.create(name: "Chuck Berry", email: "chuck_berry@rocknroll.com", company: false, phone: "650-323-2121", password: "johnnybgood")
 keith  = User.create(name: "Keith Ricards", email: "keith_richards@rocknroll.com", company: false, phone: "212-333-1212", password: "brownsugar")
-moon    = User.create(name: "Keith Moon", email: "keith_moon@underworld.com", company: true, phone: "666-666-6666", password: "whereisthebeer")
+moon    = User.create(name: "Keith Sailor Moon", email: "keith_moon@underworld.com", company: true, phone: "666-666-6666", password: "whereisthebeer")
 prince  = User.create(name: "Prince", email: "prince@strange.com", company: true, phone: "917-995-6565", password: "littleredcorvette")
