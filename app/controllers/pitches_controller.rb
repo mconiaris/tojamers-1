a4564da930e933e8a3fe76b518dcc5ddcae795ed
rnparks <rnparks@gmail.com>
2014-10-03 14:56:30 -0400

    search bar needs improvement.  index page for pitches working

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -3,11 +3,24 @@ class PitchesController < ApplicationController
 
 
   def index
+    @pitches = Pitch.all
   end
 
   def new
     @pitch = Pitch.new
   end
+
+  def create
+    binding.pry
+    # pitch = Pitch.create(user_params)
+    # pitch.save
+    # session[:user_id] = pitch.id
+    # redirect_to user_path(a)
+  end
+
+
+
+
 
 end
 
