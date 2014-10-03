5fb1e1f8db37f85e7fc0c605c05cf3dc1bc0ea39
andrewfritz86 <andrew.fritz86@gmail.com>
2014-10-03 16:43:32 -0400

    users can view their routes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,14 +1,35 @@
 class StoriesController < ApplicationController
 
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find(session[:user_id])
+    # @user = User.find(params[:user_id])
   end
 
   def new
     @story = Story.new
+    @user_id = session[:user_id]
   end
 
   def show
   end
 
+  def create
+    user = User.find(params[:userid])
+    story = Story.create(story_params)
+    story.user = user
+    story.save
+    # binding.pry
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:url, :description)
+  end
+
+
+
+
 end
+
