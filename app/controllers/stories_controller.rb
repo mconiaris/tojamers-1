dcbd3a83c9f287ab9807b135ee7bea6ee8161819
andrewfritz86 <andrew.fritz86@gmail.com>
2014-10-03 14:38:14 -0400

    shows a user's stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,14 @@
 class StoriesController < ApplicationController
 
   def index
+    @user = User.find(params[:user_id])
   end
 
   def new
     @story = Story.new
   end
+
+  def show
+  end
+
 end
