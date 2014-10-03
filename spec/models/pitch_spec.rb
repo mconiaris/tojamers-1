be60aa1fc68f949fc7f529019b04c7a606732ebd
mconiaris <mconiaris@gmail.com>
2014-10-03 09:17:32 -0400

    Added pitch_spec.rb tests.

--- spec/models/pitch_spec.rb
+++ spec/models/pitch_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Pitch, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:stories) }
+  it { should validate_presence_of(:pitch) }
 end
