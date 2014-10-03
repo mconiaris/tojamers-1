fe04e77a6399d948879291cc04f11f23aa698960
rnparks <rnparks@gmail.com>
2014-10-03 10:41:30 -0400

    cleaned up indentaiton and wrotes some code comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,26 +23,24 @@ before_action :authorize, only: [:show, :edit, :destroy, :update,]
   end
 
   private
-
+  # these are helper funcitons
   def user_params
     params.require(:user).permit(:password, :first_name, :last_name, :email, :phone, :company, :user_type)
   end
 
-    def load_user
-      @user = User.find(params[:id])
-      redirect_to root_path if !@user
-    end
+  def load_user
+    @user = User.find(params[:id])
+    redirect_to root_path if !@user
+  end
 
-    def authenticate
-      redirect_to root_path if !logged_in?#if the sessions u?er is returning nil, let's redirect to login
-    end
+  def authenticate
+    redirect_to root_path if !logged_in?#if the sessions u?er is returning nil, let's redirect to login
+  end
 
-    def authorize
-
-      if current_user != @user #here we are making sure that the user coming from params is the same as the user in params
+  def authorize
+    if current_user != @user #here we are making sure that the user coming from params is the same as the user in params
       redirect_to user_path(current_user)
-      end
-
     end
+  end
 
 end
