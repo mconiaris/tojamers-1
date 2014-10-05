b64bd5b3a066a45daf093681b7b7b8cd56ac8b07
rnparks <rnparks@gmail.com>
2014-10-05 14:47:36 -0400

    random story generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-c# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
@@ -31,8 +31,12 @@ def random_user_individual
   User.create(first_name: first_name, last_name: last_name, email: email, phone: phone, user_type: user_type, password: password)
 end
 
+def random_story
+  Story.create(user_id: rand(1..50), url: Faker::Internet.url, description: Faker::Company.catch_phrase)
+end
+
 def random_pitch
-  Pitch.create(story_id: "#{rand(1..3).to_s}", body: "#{Faker::Lorem.sentence}")
+  Pitch.create(story_id: "#{rand(1..100).to_s}", body: "#{Faker::Lorem.sentence}")
 end
 
 
@@ -40,12 +44,6 @@ ryan = User.create(first_name: "Ryan", last_name: "Parks", email: "rnparks@gmail
 pj = User.create(first_name: "PJ", last_name: "Hughes", email: "pj@ga.co", phone: "(855) 288-0881", user_type: "company", password: "thisisfake")
 phil = User.create(first_name: "Phil", last_name: "Lamplaugh", email: "philco@ga.co", phone: "(855) 288-0881", user_type: "individual", password: "bullshitpassword")
 travis = User.create(first_name: "Travis", last_name: "Vander Hoop", email: "trav@ga.co", phone: "(855) 288-0881", user_type: "individual", password: "notrealatall")
-
-#generating random users
-25.times do
-  random_user_company
-  random_user_individual
-end
 
 pj_story = Story.create(user_id: 1, url: "https://generalassemb.ly/", description: "GA is building a Website for people who are in a bad place in their life." )
 phil_story = Story.create(user_id: 2, url: "https://www.linkedin.com/pub/phillip-lamplugh/3/7ab/986", description: "I am a Brooding Visionaire." )
@@ -64,7 +62,18 @@ pj_pitch.save
 phil_pitch.save
 travis_pitch.save
 
+#generating random users
 25.times do
+  random_user_company
+  random_user_individual
+end
+
+100.times do
+  random_story
+end
+
+
+250.times do
   random_pitch
 end
 
