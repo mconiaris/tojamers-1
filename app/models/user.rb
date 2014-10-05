f96f58af4674179c0541b1763863812ad651483b
rnparks <rnparks@gmail.com>
2014-10-05 14:52:23 -0400

    validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
  has_secure_password
  has_many :stories
  has_many :pitches, through: :stories
+ validates :email, presence: true, uniqueness: true
+ validates :first_name, presence: true
+ validates :last_name, presence: true
 end
