d21894d527bc6114210507212c67004a8719b391
mconiaris <mconiaris@gmail.com>
2014-10-06 16:50:37 -0400

    Fixed user test.

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe "users/edit", :type => :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      :first_name => "MyString",
-      :last_name => "MyString",
-      :email => "MyString",
-      :phone => "MyString"
+      :first_name   => "MyString",
+      :last_name    => "MyString",
+      :email        => "MyString",
+      :phone        => "MyString",
+      :password     => "MyString"
     ))
   end
 
-  xit "renders the edit user form" do
+  it "renders the edit user form" do
     render
 
     assert_select "form[action=?][method=?]", user_path(@user), "post" do
